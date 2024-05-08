e3cba3d40efef0f60acb5469b00a58a267776c85
gregorio2311 <gregorio.rico.gregorio@gmail.com>
2024-05-07 20:16:09 -0600

    Capa service
    Modifica el modelo Alumno:
    
    Agregar las siguientes propiedades junto con sus métodos get y set:
    telefono
    email
    
    Agrega los siguientes endpoints a tu microservicio:
    
    UpdateAlumno: el usuario usará este endpoint para modificar la información de un alumno en específico, será utilizado para cambiar por completo la información del Alumno por lo que será necesario que definas el método HTTP más adecuado; si la operación fue correcta, responde un estatus 200, si el alumno del cual se intenta modificar la información no existe, responde un error 404.
    
    DeleteAlumno: el usuario usará este endpoint para eliminar un alumno en específico del listado, será necesario que definas el método HTTP más adecuado; si la operación fue correcta, responde un estatus 200, si el alumno del cual se intenta modificar la información no existe, responde un error 404.

--- src/main/java/com/fcm/backend/model/Alumno.java
+++ src/main/java/com/fcm/backend/model/Alumno.java
@@ -6,6 +6,9 @@ public class Alumno {
     private String apellidoPat;
     private String apellidoMat;
     private String fechaNac;
+    private String telefono;
+    private String email;
+
 
     public String getNombre() {
         return nombre;
@@ -40,4 +43,19 @@ public class Alumno {
     }
 
 
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
